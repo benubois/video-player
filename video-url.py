910e89c503e1def39e7f37029e293873c492058b
Ben Ubois <ben@benubois.com>
2018-11-10 19:20:19 -0800

    Update video-url.py

--- video-url.py
+++ video-url.py
@@ -1,30 +1,29 @@
 import youtube_dl
-import json
 import sys
 import urllib
 from objc_util import nsurl,UIApplication
 
 ydl_opts = {
-	'format': 'best[ext=mp4]/best',
-	'simulate': True,
-	'quiet': True,
-	'forceurl': True,
+    'format': 'best[ext=mp4]/best',
+    'simulate': True,
+    'quiet': True,
+    'forceurl': True,
 }
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	info = ydl.extract_info(sys.argv[1], download = False)
+    info = ydl.extract_info(sys.argv[1], download = False)
 	
-	base_url = 'https://watchable-video.github.io/player'
-	video_url = info['url']
-	title = info['title']
-	poster = info['thumbnail']
+    base_url = 'https://watchable-video.github.io/player'
+    video_url = info['url']
+    title = info['title']
+    poster = info['thumbnail']
 	
-	args = {
-		'url': video_url, 
-		'poster': poster,
-		'title': title
+    args = {
+        'url': video_url, 
+        'poster': poster,
+        'title': title
 	}
-	url = base_url + "?" + urllib.parse.urlencode(args)
+	url = base_url + '?' + urllib.parse.urlencode(args)
 	
 	app = UIApplication.sharedApplication()
 	app.openURL_(nsurl(url))
